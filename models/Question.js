543de8b8df8aa6b41ab290b051da718bfdc29c9b
Avi Flombaum <avi@flombaum.com>
2017-11-16 05:30:14 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -3,14 +3,11 @@
 const db = require("../config/db")
 
 class Question{
-  class Question{
-    static CreateTable() {
-      return new Promise(function(resolve){
-        resolve("This Does Nothing!")
-      })
-    }
+  static CreateTable() {
+    return new Promise(function(resolve){
+      resolve("This Does Nothing!")
+    })
   }
 }
-
 
 module.exports = Question;
